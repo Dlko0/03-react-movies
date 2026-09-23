235a9d4d3ce8a14db22e8cddb33f1e3c84ef572d
Danylo Alienin <danya120805@gmail.com>
2026-09-23 19:01:17 +0200

    fix bug

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,3 @@
-import type { FormEvent } from 'react';
 import { toast } from 'react-hot-toast';
 import css from './SearchBar.module.css';
 
@@ -7,10 +6,7 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
+  const handleSubmitAction = (formData: FormData) => {
     const query = (formData.get('query') ?? '').toString().trim();
 
     if (!query) {
@@ -19,7 +15,6 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
     }
 
     onSubmit(query);
-    event.currentTarget.reset();
   };
 
   return (
@@ -34,7 +29,10 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
           Powered by TMDB
         </a>
 
-        <form className={css.form} onSubmit={handleSubmit}>
+        <form
+          className={css.form}
+          action={handleSubmitAction as unknown as string}
+        >
           <input
             className={css.input}
             type="text"
